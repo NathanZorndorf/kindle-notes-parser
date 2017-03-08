52514ad90d239a71086a00638d1444070ce6c3a8
Nathan Zorndorf <nzorndorf@gmail.com>
2017-03-07 19:33:04 -0800

    committing to push

--- Kindle+Notes+Parser.py
+++ Kindle+Notes+Parser.py
@@ -1,3 +1,39 @@
+
+# coding: utf-8
+
+# In[2]:
+
+import os.path
+
+
+# In[ ]:
+
+import 
+
+
+# In[19]:
+
+clipping = open('Kindle Clippings 11:16.txt', 'r')
+
+
+# In[11]:
+
+new_notes = open('Kindle Paperwhite Notes.txt', 'w')
+
+
+# In[20]:
+
+for line in clipping:
+    print(line)
+
+
+# In[23]:
+
+clipping.close()
+
+
+# In[7]:
+
 import re
 import os.path
 import tkinter as tk
@@ -53,25 +89,47 @@ with open('My Clippings.txt') as clipping:
             prevTextType = textType
             if 'Highlight' in line:
                 textType = 'Highlight'
+                location = line.split(' ')
+                location = location[5]
+                location = 'Location ' + str(location)
             elif('Note' in line):
                 textType = 'Note'
+                location = line.split(' ')
+                location = location[5]
+                location = 'Location ' + str(location)
+                
                 
         if(lineNum == 3): # this line contains the highlighted text, or the note
-            text = line[:-1] # don't need the newline at the end            
+            text = line[:-1] # don't need the newline at the end   
+            if(textType == 'Note'):
+                note = text 
+            elif(textType == 'Highlight'):
+                quote = text
         
         lineNum += 1 # increment our lineNum variable                 
         
         if '==========' in line: # if we reach the end of a highlight/note
             lineNum = 0 # reset lineNum
                 
-            if(textType == 'Highlight'): # if it's a higlight, warp with quotation marks, append two newline characters
-                s = 'Quote: ' + '\"' + text + '\"' + '\n'
-                if(prevTextType == 'Highlight'): # add another 2 newlines if the previous entry was a quote/highlight
-                    s = '\n\n' + s                
-            else: # if it's a note, append three newlines
-                s = 'Note: ' + text + '\n\n\n'
+            if(textType == 'Highlight'): # if it's a higlight, wrap with quotation marks, append two newline characters
+                s = '\n\n\n' + 'Quote at ' + location + ': ' + '\"' + quote + '\"'
+                curFile.write(s)
+                    
+            if(prevTextType == 'Note'): # if the previous text was a note, then add the note AFTER the quote
+                s =  '\n' + 'Note at ' + location + ': ' + note  
+                curFile.write(s)
                 
-            curFile.write(s) # then insert the text into the appropriate file     
-            curFile.close()
+                
+            curFile.close() # close the current file, because the next item in 'My Clippings.txt' could be for a different book
             
             
+
+
+# In[ ]:
+
+testFile = open('Text Formatting Test.rtf', 'w')
+
+quote = "To thine own self be true, and it must follow, as the night the day, thou canst not then be false to any man."
+text = "That's what I'm sayin dawg!!"
+
+
